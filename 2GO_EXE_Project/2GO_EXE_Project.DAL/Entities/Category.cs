dfa3bf73dd08ef0ba530b8d80421ad2c53beda3f
Kimpung2901 <phungkimthi29012004@gmail.com>
2025-12-11 23:30:23 +0700

    Add Entity Framework Core DAL and entities
    Introduced Entity Framework Core to the DAL project with SQL Server support. Added AppDbContext and entity classes for core domain models, including user, listing, order, payment, chat, and related entities. Updated project files to include necessary EF Core packages.

---
+++ 2GO_EXE_Project/2GO_EXE_Project.DAL/Entities/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2GO_EXE_Project.DAL.Entities;
+
+public partial class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+
+    [StringLength(255)]
+    [Unicode(false)]
+    public string? Name { get; set; }
+
+    [StringLength(255)]
+    [Unicode(false)]
+    public string? IconUrl { get; set; }
+
+    [InverseProperty("Category")]
+    public virtual ICollection<SubCategory> SubCategories { get; set; } = new List<SubCategory>();
+}
